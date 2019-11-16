f37ad083d005af8459044a9d3d5ccbb711c7ace7
jennylember <jane.akhunzyanova@gmail.com>
2019-11-16 12:36:24 +0300

    Add function mul.

--- src/Main.kt
+++ src/Main.kt
@@ -3,9 +3,13 @@
 fun main() {
     println("Hello Git!")
     println("Sum: ${add(10, 50)}.")
+    println("Mul: ${mul(10, 3)}.")
 }
-
 
 fun add(a: Int, b: Int): Int {
     return a + b
+}
+
+fun mul(a: Int, b: Int): Int {
+    return a * b
 }
